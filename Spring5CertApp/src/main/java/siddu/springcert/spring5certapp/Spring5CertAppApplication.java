ce3693ce6d8389d6952a8a25b0fd1ba5a14c8096
I312117 <shanmukha.siddartha.cherukumudi@sap.com>
2019-10-15 23:45:44 +0530

    Udemy Spring Course First commit

---
+++ Spring5CertApp/src/main/java/siddu/springcert/spring5certapp/Spring5CertAppApplication.java
@@ -0,0 +1,13 @@
+package siddu.springcert.spring5certapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5CertAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5CertAppApplication.class, args);
+	}
+
+}
