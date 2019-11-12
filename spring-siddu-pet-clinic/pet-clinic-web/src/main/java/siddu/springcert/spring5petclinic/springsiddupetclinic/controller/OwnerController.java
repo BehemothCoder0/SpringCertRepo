0d9f5f45dfa6db2d0f2fa05b4600338a0de59dc7
I312117 <shanmukha.siddartha.cherukumudi@sap.com>
2019-11-12 13:49:17 +0530

    Create Owner index page and controller closes #14

---
+++ spring-siddu-pet-clinic/pet-clinic-web/src/main/java/siddu/springcert/spring5petclinic/springsiddupetclinic/controller/OwnerController.java
@@ -0,0 +1,13 @@
+package siddu.springcert.spring5petclinic.springsiddupetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+	@RequestMapping({"","/index","/index.html"})
+	public String listOwners() {
+		return "owners/index";
+	}
+}
