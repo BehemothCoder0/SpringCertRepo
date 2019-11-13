7bd31e07b65536cb412ecd526c9065c95c64ea0c
I312117 <shanmukha.siddartha.cherukumudi@sap.com>
2019-11-13 16:19:41 +0530

    List all Owners and Vets closes #17 and closes #16

--- spring-siddu-pet-clinic/pet-clinic-web/src/main/java/siddu/springcert/spring5petclinic/springsiddupetclinic/controller/VetController.java
+++ spring-siddu-pet-clinic/pet-clinic-web/src/main/java/siddu/springcert/spring5petclinic/springsiddupetclinic/controller/VetController.java
@@ -1,13 +1,29 @@
 package siddu.springcert.spring5petclinic.springsiddupetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import siddu.springcert.spring5petclinic.springsiddupetclinic.service.VetService;
+
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 	
-	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVets() {
+	private final VetService vetService;
+	
+	
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+
+
+	@RequestMapping({"","/index","/index.html"})
+	public String listVets(Model model) {
+		model.addAttribute("vets",vetService.findAll());
 		return "vets/index";
 	}
 }
